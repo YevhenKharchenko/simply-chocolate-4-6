f5961015a7008e295a6c52146da34b37cd35ab57
YevhenKharchenko <evgenvalerych@gmail.com>
2023-12-29 11:05:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 const reviewBtn = document.querySelector(".reviews-btn");
 const backdrop = document.querySelector(".backdrop");
 
-reviewBtn.addEventListener("click", () => {
+reviewBtn.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
   backdrop.classList.add("is-open");
-});
+}
 
 const closeBtn = document.querySelector(".modal-close-btn");
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click", onCloseButtonClick);
+
+function onCloseButtonClick() {
   backdrop.classList.remove("is-open");
-});
+}
